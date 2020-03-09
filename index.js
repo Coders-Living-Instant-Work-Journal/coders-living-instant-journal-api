09ef136adec506d467b5f026416ea56e5e15039d
Eugene Monnier <eugene.monnier@gmail.com>
2020-03-09 14:20:25 -0700

    added routes for update and delete

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ require('dotenv').config()
 const { PORT, MONGODB_URI } = process.env
 const mongoose = require('mongoose')
 
-const Journal = require('./src/lib/entry')
-const Input = require('./src/lib/input')
-
-const input = new Input()
-const entry = new Journal(input)
-
-entry.execute(input)
-
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
