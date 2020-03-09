709c4fe8dda9f6b55fd34d2e96ae98180202365e
Eugene Monnier <eugene.monnier@gmail.com>
2020-03-09 16:04:42 -0700

    add tests for create route

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,5 @@
 function errorHandler (err, req, res, next) {
-  console.error('___SERVER ERROR___', err)
+  // console.error('___SERVER ERROR___', err)
   res.status(500).json({ error: err.message })
 }
 
