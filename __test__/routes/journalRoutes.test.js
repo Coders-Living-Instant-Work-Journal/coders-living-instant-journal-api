017939ea9483d532712a2fd1c31da677ad1826c6
Eugene Monnier <eugene.monnier@gmail.com>
2020-03-09 17:17:23 -0700

    added tests for create and read routes

--- __test__/routes/journalRoutes.test.js
+++ __test__/routes/journalRoutes.test.js
@@ -3,13 +3,16 @@ const supergoose = require('@code-fellows/supergoose')
 const { server } = require('../../src/app')
 const mockRequest = supergoose(server)
 
+let entryId
+
 describe('journalRoutes module:', () => {
-  describe('create route', () => {
+  describe('Create route', () => {
     test('Create a new entry in MongoDB', () => {
       return mockRequest
         .post('/create')
         .send({ category: 'journalRoutesTest', text: 'attaches to the ships unbreakable diamond' })
         .then(results => {
+          entryId = results.body.entry._id
           expect(results.status).toBe(200)
           expect(results.body.entry.category).toEqual('journalRoutesTest')
           expect(results.body.entry.text).toEqual('attaches to the ships unbreakable diamond')
@@ -21,7 +24,6 @@ describe('journalRoutes module:', () => {
         .post('/create')
         .send({ category: 'this will fail' })
         .then(results => {
-          console.log(results.error.text)
           expect(results.error.text).toEqual('{"error":"entry validation failed: text: Path `text` is required."}')
         })
     })
@@ -31,7 +33,6 @@ describe('journalRoutes module:', () => {
         .post('/create')
         .send({ text: 'this will fail' })
         .then(results => {
-          console.log(results.error.text)
           expect(results.error.text).toEqual('{"error":"entry validation failed: category: Path `category` is required."}')
         })
     })
@@ -42,6 +43,38 @@ describe('journalRoutes module:', () => {
         .send({ })
         .then(results => {
           expect(results.error.text).toEqual('{"error":"entry validation failed: text: Path `text` is required., category: Path `category` is required."}')
+        })
+    })
+  })
+
+  describe('Read route', () => {
+    test('Return all entries', () => {
+      mockRequest
+        .post('/create')
+        .send({ category: 'readRouteId', text: 'you got me by my IDs' })
+        .then()
+      return mockRequest
+        .get('/read')
+        .then(results => {
+          expect(results.body.length).toEqual(2)
+        })
+    })
+
+    test('Return all entries of given category', () => {
+      return mockRequest
+        .get('/read')
+        .send({ category: 'journalRoutesTest' })
+        .then(results => {
+          expect(results.body[0].text).toEqual('attaches to the ships unbreakable diamond')
+        })
+    })
+
+    test('Return entry of given id', () => {
+      return mockRequest
+        .get('/read')
+        .send({ _id: entryId })
+        .then(results => {
+          expect(results.body[0].text).toEqual('attaches to the ships unbreakable diamond')
         })
     })
   })
